40d8164ceac7f9b69302cb1b80d0b85189416463
Tomy Falgui <tomyfalgui@gmail.com>
2023-08-28 11:50:02 +0800

    test: update msw endpoint

--- client/__tests__/puppy-display.test.tsx
+++ client/__tests__/puppy-display.test.tsx
@@ -3,13 +3,13 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import {puppyList} from '../testdata/puppy';
-import Home from '../app/page';
+import { puppyList } from '../testdata/puppy'
+import Home from '../app/page'
 
 const server = setupServer(
-    rest.get('/api/puppy', (req,res,ctx) => {
-        return res(ctx.json(puppyList))
-    })
+  rest.get('http://localhost:5555/api/puppy', (req, res, ctx) => {
+    return res(ctx.json(puppyList))
+  })
 )
 
 beforeAll(() => server.listen())
@@ -17,25 +17,23 @@ afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 describe('puppy rendering', () => {
-    test('Home should have h1 header', async () => {
-        render(<Home />);
+  test('Home should have h1 header', async () => {
+    render(<Home />)
 
-        await screen.findByRole('heading')
+    await screen.findByRole('heading')
 
-        expect(screen.getByRole('heading')).toHaveTextContent(/adopt a puppy/i)
+    expect(screen.getByRole('heading')).toHaveTextContent(/adopt a puppy/i)
+  })
 
+  test('loads and displays puppies', async () => {
+    render(<Home />)
+
+    const puppies = await screen.findAllByRole('listitem')
+    puppies.forEach((puppy, index) => {
+      const puppyName = puppyList[index].name
+      const puppyNameEl = screen.getByText(puppyName)
+
+      expect(puppy).toContainElement(puppyNameEl)
     })
-
-    test('loads and displays puppies', async () => {
-        render(<Home/>)
-
-        const puppies = await screen.findAllByRole('listitem')
-        puppies.forEach((puppy, index) => {
-            const puppyName = puppyList[index].name;
-            const puppyNameEl = screen.getByText(puppyName)
-
-            expect(puppy).toContainElement(puppyNameEl)
-        })
-
-    })
+  })
 })
