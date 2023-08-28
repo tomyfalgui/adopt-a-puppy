0430a600f57047033bec2e06fe8b06f4bcccd867
Tomy Falgui <tomyfalgui@gmail.com>
2023-08-28 13:22:25 +0800

    test: initial test for breed options

--- client/__tests__/puppy-display.test.tsx
+++ client/__tests__/puppy-display.test.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
-import { render, screen } from '@testing-library/react'
+import { render, screen, within } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { puppyList } from '../testdata/puppy'
 import Home from '../app/page'
+import exp from 'constants'
 
 const server = setupServer(
   rest.get('http://localhost:5555/api/puppy', (req, res, ctx) => {
     return res(ctx.json(puppyList))
-  })
+  }),
+  rest.get(
+    'http://localhost:5555/api/puppy/filter-options',
+    (req, res, ctx) => {
+      return res(
+        ctx.json({
+          breeds: [
+            'Jack Russell',
+            'Collie cross',
+            'Labrador',
+            'Springer Spaniel',
+          ],
+          ages: [1, 2, 3],
+          genders: ['male', 'female'],
+          sizes: ['small', 'medium', 'large'],
+        })
+      )
+    }
+  )
 )
 
 beforeAll(() => server.listen())
@@ -34,6 +53,28 @@ describe('puppy rendering', () => {
       const puppyNameEl = screen.getByText(puppyName)
 
       expect(puppy).toContainElement(puppyNameEl)
+    })
+  })
+})
+
+describe('filter options', () => {
+  test('should fetch and render specific breed options', async () => {
+    render(<Home />)
+
+    const selectBreed = screen.getByRole('combobox', {
+      name: /select a breed/i,
+    })
+    const breedOptions = await within(selectBreed).findAllByRole('option')
+    const breedOptionTexts = breedOptions.map(opt => opt.textContent)
+
+    const expectedBreeds = [
+      'Jack Russell',
+      'Collie cross',
+      'Labrador',
+      'Springer Spaniel',
+    ]
+    expectedBreeds.forEach(opt => {
+      expect(breedOptionTexts).toContain(opt)
     })
   })
 })
