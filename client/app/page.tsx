d09e3f572dd87796faf8707afa37dc6656035050
Tomy Falgui <tomyfalgui@gmail.com>
2023-08-28 10:18:18 +0800

    refactor: add basic styling

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,30 +1,37 @@
 'use client'
 
-import {useState, useEffect} from 'react'
-import PuppyDisplay from "../components/Puppy";
-import {Puppy} from "@/types/puppy";
+import { useState, useEffect } from 'react'
+import PuppyDisplay from '../components/Puppy'
+import { Puppy } from '@/types/puppy'
 
 export default function Home() {
-    const [data, setData] = useState<Puppy[]>([])
+  const [data, setData] = useState<Puppy[]>([])
 
-    useEffect(() => {
-        fetch('http://localhost:5555/api/puppy')
-            .then(res => res.json())
-            .then(data => {
-                setData(data)
-            })
-    }, []);
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Adopt A Puppy</h1>
+  useEffect(() => {
+    fetch('http://localhost:5555/api/puppy')
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+      })
+  }, [])
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-5xl mb-10">Adopt A Puppy</h1>
 
-            {data && data.length > 0 ?
-                <ul>
-                    {data.map(({name, photoUrl, breed}, idx) => {
-                        return <PuppyDisplay name={name} photoUrl={photoUrl} breed={breed} key={idx}/>
-                    })}
-                </ul>
-                : null}
-        </main>
-    )
+      {data && data.length > 0 ? (
+        <ul className="flex flex-wrap">
+          {data.map(({ name, photoUrl, breed }, idx) => {
+            return (
+              <PuppyDisplay
+                name={name}
+                photoUrl={photoUrl}
+                breed={breed}
+                key={idx}
+              />
+            )
+          })}
+        </ul>
+      ) : null}
+    </main>
+  )
 }
--- client/components/Puppy.tsx
+++ client/components/Puppy.tsx
@@ -1,12 +1,17 @@
 export type PuppyProps = {
-    name: string;
-    photoUrl: string;
-    breed: string;
+  name: string
+  photoUrl: string
+  breed: string
 }
-export default function PuppyDisplay({name, photoUrl, breed}: PuppyProps) {
-    return (<li>
-        <img src={photoUrl} alt={`An image of puppy breed ${breed}`}/>
-        <p>{name}</p>
-    </li>)
-
+export default function PuppyDisplay({ name, photoUrl, breed }: PuppyProps) {
+  return (
+    <li className="mr-8 flex flex-col items-center">
+      <img
+        src={photoUrl}
+        alt={`An image of puppy breed ${breed}`}
+        className="object-cover w-40 h-40 rounded-full"
+      />
+      <p className="text-2xl">{name}</p>
+    </li>
+  )
 }
